d903304a3fc73da0cd10267793dd096b5f26a581
Juan2209 <teodoropavonjuan@gmail.com>
2024-10-14 22:04:03 -0600

    TAREA 14/10/2024

--- control_usuario/app.js
+++ control_usuario/app.js
@@ -54,6 +54,21 @@ app.post("/usuarios", (req, res)=>{
     1.-La informacion debe estar completa, si una de ellas no llega enviar un error (400)
     2.-El email debe ser unico (400) */
     const {nombre, apellido, email} = req.body;
+    if (!nombre || nombre.trim() === '') {
+        return res.status(400).send({ error: "Nombre es un campo obligatorio" });
+    }
+    if (!apellido || apellido.trim() === '') {
+        return res.status(400).send({ error: "Apellido es un campo obligatorio" });
+    }
+    if (!email || email.trim() === '') {
+        return res.status(400).send({ error: "Email es un campo obligatorio" });
+    }
+
+    const emailExists = usuarios.some(usuario => usuario.email === email);
+    if (emailExists) {
+        return res.status(400).send({ error: "El email ya está en uso" });
+    }
+
     usuarios.push({id: usuarios.length + 1, nombre, apellido, email});
     //console.log(req.body)
     res.status(201).send("El usuario se agregó correctamente");
