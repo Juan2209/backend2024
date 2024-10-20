baf49337e783495beb4493e2e4de16d80d9ac9e4
Juan2209 <teodoropavonjuan@gmail.com>
2024-10-20 07:28:02 -0600

    Clase del 18/10/2024

--- control_usuario/app.js
+++ control_usuario/app.js
@@ -117,9 +117,68 @@ app.put("/usuarios/:id", (req, res) =>{
 });
 //TAREA DE VALIDACION: solamente me deje actualizar el correo siempre y cuando no exista en otro usuario
 //solo modfica una parte de la informacion del usuario
+//CLASE DEL VIERNES 18/10/2024
 app.patch("/usuarios/:id", (req, res) =>{
-
+    const nuevaInfoUsuario = req.body;
+    const id = req.params.id;
+  
+    //Realizamos la validación de los parámetros
+    if(isNaN(id)){
+      res.status(400).send({error: "El id debe ser un número"});
+      return;
+    };
+    
+    const usuario = usuarios.find((usuario) => usuario.id === +id);
+  
+    if(!usuario){
+      res.status(404).send({error: `El usuario con id ${id} no existe`});
+      return;
+    };
+    //Se revisa si se envió algún dato para actualizar, importante el uso de parentesis para que se evalúe la expresión
+    if(!(nuevaInfoUsuario.nombre || nuevaInfoUsuario.apellido || nuevaInfoUsuario.email)){
+      res.status(400).send({error: "Debe enviar información para actualizar el usuario"});
+      return;
+    }
+  
+    if(nuevaInfoUsuario.email && usuarios.find((usuario) => usuario.email === nuevaInfoUsuario.email) && nuevaInfoUsuario.email !== usuarios[+id].email){
+      res.status(400).send({error: "El email ya existe"});
+      return;
+    }
+  
+    // Se actualizan los datos del usuario, si no se envió ningún dato se mantiene el mismo, usando el operador || para que se evalúe la expresión
+    usuarios.forEach((usuario) => {
+      if(usuario.id === +id){
+        usuario.nombre = nuevaInfoUsuario.nombre || usuario.nombre;
+        usuario.apellido = nuevaInfoUsuario.apellido || usuario.apellido;
+        usuario.email = nuevaInfoUsuario.email || usuario.email;
+      }
+    })
+  
+    res.status(200).send("El usuario se actualizó correctamente");
 });
+app.delete("/usuarios/:id", (req, res) => {
+    const id = req.params.id;
+  
+    // Realizamos la validación de los parámetros
+    // Como no se recibe ningún parámetro, el número de validaciones es menor
+    if(isNaN(id)){
+      res.status(400).send({error: "El id debe ser un número"});
+      return;
+    };
+    
+    const usuario = usuarios.find((usuario) => usuario.id === +id);
+  
+    if(!usuario){
+      res.status(404).send({error: `El usuario con id ${id} no existe`});
+      return;
+    };
+  
+    // Se elimina el usuario
+    // El método splice elimina el usuario en la posición que corresponda, y devuelve el elemento eliminado
+    usuarios.splice(usuarios.findIndex((usuario) => usuario.id === +id), 1);
+  
+    res.status(200).send("El usuario se eliminó correctamente");
+  });
 app.listen(3000, ()=>{
     console.log("Servidor corriendo en http://localhost:3000");
 });
