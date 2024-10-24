3051471a54488881565fbb032c99180f4700bb9b
Juan2209 <teodoropavonjuan@gmail.com>
2024-10-24 12:01:42 -0600

    clase del 24/10/2024

--- stock_manager/src/server.js
+++ stock_manager/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const usersRoutes = require('./routes/users');
 
 
 class Server {
@@ -6,7 +7,13 @@ class Server {
         this.app = express();
         this.port = 3000;
         this.app.use(express.json());
+        this.routes();
     }
+
+    routes(){
+        this.app.use('/users', usersRoutes);
+    }
+
     start(){
         this.app.listen(this.port, () =>{
             console.log('Server listeninig on port' + this.port);
