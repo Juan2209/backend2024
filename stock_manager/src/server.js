d87ee3f36cd5ffee7f9f04710906d9806643a9b9
Juan2209 <teodoropavonjuan@gmail.com>
2024-11-26 21:52:40 -0600

    TAREA DE ENDPOINTS

--- stock_manager/src/server.js
+++ stock_manager/src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const usersRoutes = require('./routes/users');
+const productSuppliersRoutes = require('./routes/products_suppliers'); // Importa las rutas de products_suppliers
 const staffRoutes = require('./routes/staff');
 const suppliersRoutes = require('./routes/suppliers');
 const productsRoutes = require('./routes/products');
+const purchasesRoutes = require('./routes/purchases');
 
 class Server {
     constructor() {
@@ -19,9 +21,11 @@ class Server {
 
     routes(){
         this.app.use('/users', usersRoutes);
+        this.app.use('/products_suppliers', productSuppliersRoutes); // Rutas de products_suppliers
         this.app.use('/staff', staffRoutes); // Nueva ruta para staff
         this.app.use('/products', productsRoutes); // Nueva ruta para productos
         this.app.use('/suppliers', suppliersRoutes);
+        this.app.use('/purchases', purchasesRoutes);
     }
 
     start(){
